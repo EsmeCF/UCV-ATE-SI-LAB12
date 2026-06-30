8a3423ab61adf95efc51ffc2153218b352f06443
Escordovaf <escordovaf@ucvvirtual.edu.pe>
2026-06-29 23:43:48 -0500

    Laboratorio Perceptrón Simple - UCV

---
+++ tests/test_perceptron.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from src.perceptron import Perceptron
+from src.dataset import training_data
+
+def test_prediction():
+    model = Perceptron()
+    model.train(training_data)
+    assert model.predict([8,1]) == 1
+
+def test_rejection():
+    model = Perceptron()
+    model.train(training_data)
+    assert model.predict([1,0]) == 0
